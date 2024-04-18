342b487d3f8971cf6387bc89c8c66360a23301ae
JaiJunior <jaijunior11@gmail.com>
2024-04-18 20:56:59 -0300

    Dividindo o programa em funções

--- main.py
+++ main.py
@@ -3,53 +3,61 @@ menu = """
 	[2] - Saque
 	[3] - Consultar Extrato
 """
-MAX_SAQUE = 500
-LIMITE_SAQUES = 3
+SAQUE_MAXIMO 	= 500
+LIMITE_SAQUES 	= 3
 
-saques = 0
-saldo = 500
-extrato = []
-valor = 0
+saques	= 0
+saldo 	= 500
+extrato	= []
+
+def deposito(valor: float):
+	if valor > 0:
+		global saldo
+		saldo += valor
+		extrato.append(f"|Deposito realizado no valor de R${valor:.2f}\n|Saldo -------> R${saldo:.2f}|")
+		return f"Deposito de R${valor:.2f} realizado com sucesso!"
+	else:
+		return "Insira um valor maior do que 0(Zero)"
+
+def saque(valor: float):
+	if valor <= 0:
+		return "Insira um valor maior do que 0(Zero)"
+
+	if valor > SAQUE_MAXIMO:
+		return f"O límite por saque é igual a {SAQUE_MAXIMO}, tente novamente"
+
+	global saques
+	if saques > 2:
+		return f"Você ja realizou {saques} saques que é o limite"
+
+	global saldo
+	if (saldo - valor) < 0:
+		return f"Você não tem saldo suficiente para o saque"
+
+	saldo -= valor
+	extrato.append(f"|Saque realizado no valor de R${valor:.2f}\m|Saldo -------> R${saldo:.2f}|")
+	saques += 1
+	return f"Saque de R${valor:.2f} realizado com sucesso"
+
+def mostraExtrato(extrato):
+	print("|-----------------------------------------|")
+	print("|---------------EXTRATO-------------------|")
+	for x in extrato:
+		print(f"{x}\n")
 
 
 while True:
 	opcao = input(menu)
 
 	if opcao == "1":
-		valor = float(input("Informe o valor do Deposito:"))
-
-		if valor > 0:
-			saldo += valor
-			extrato.append(f"|Deposito realizado no valor de R${valor:.2f}\n|Saldo ---------> {saldo:.2f}|")
-			print(f"Deposito no valor de R${valor:.2f} realizado com sucesso")
-			valor = 0;
-
-		else:
-			print("Insira um valor maior do que 0(zero)")
+		valor = float(input("Informe o valor do Deposito: "))
+		print(deposito(valor))
 
 	elif opcao == "2":
-		valor = float(input("Informe o valor do Saque:"))
-
-		if (valor > 0) and (saldo - valor) > 0:
-
-			if valor <= MAX_SAQUE and saques <=2:
-				saldo -= valor
-				extrato.append(f"|Saque realizado no valor de R${valor:.2f}\n|Saldo --------> {saldo:.2f}|")
-				print(f"Saque no valor de R${valor:.2f} realizado com sucesso")
-				saques += 1
-				valor = 0
-			else:
-				print(f"Você só pode sacar valor até R${MAX_SAQUE} e no máximo 3 saques")
-		elif (saldo - valor) < 0:
-			print(f"Você não tem R${valor:.2f} para sacar, seu saldo atual é de R${saldo:.2f}. Tente novamente")
-
-		else:
-			print("Insira um valor maior do que 0")
+		valor = float(input("Informe o valor do Saque: "))
+		print(saque(valor))
 
 	elif opcao == "3":
-		print("|-----------------------------------------|")
-		print("|---------------EXTRATO-------------------|")
-		for x in extrato:
-			print(f"{x}\n")
+		mostraExtrato(extrato)
 
 
